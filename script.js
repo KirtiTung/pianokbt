58c9c1047f2fc366c349d2670dfa5e6c4d93a31f
KirtiTung <70936047+KirtiTung@users.noreply.github.com>
2021-09-06 00:41:46 +0530

    Add initial piano website files

---
+++ script.js
@@ -0,0 +1,166 @@
+function stopSong(note){
+    note.pause()
+    note.currentTime=1;
+}
+
+document.getElementById('cl').addEventListener('click',()=>{
+
+    const note=document.getElementById('cljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('dl').addEventListener('click',()=>{
+
+    const note=document.getElementById('dljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('el').addEventListener('click',()=>{
+
+    const note=document.getElementById('eljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('fl').addEventListener('click',()=>{
+
+    const note=document.getElementById('fljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('gl').addEventListener('click',()=>{
+
+    const note=document.getElementById('gljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('al').addEventListener('click',()=>{
+
+    const note=document.getElementById('aljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('bl').addEventListener('click',()=>{
+
+    const note=document.getElementById('bljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('c').addEventListener('click',()=>{
+
+    const note=document.getElementById('cjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('d').addEventListener('click',()=>{
+
+    const note=document.getElementById('djs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('e').addEventListener('click',()=>{
+
+    const note=document.getElementById('ejs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('f').addEventListener('click',()=>{
+
+    const note=document.getElementById('fjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('g').addEventListener('click',()=>{
+
+    const note=document.getElementById('gjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('a').addEventListener('click',()=>{
+
+    const note=document.getElementById('ajs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('b').addEventListener('click',()=>{
+
+    const note=document.getElementById('bjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('csl').addEventListener('click',()=>{
+
+    const note=document.getElementById('csljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('dsl').addEventListener('click',()=>{
+
+    const note=document.getElementById('dsljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('fsl').addEventListener('click',()=>{
+
+    const note=document.getElementById('fsljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('gsl').addEventListener('click',()=>{
+
+    const note=document.getElementById('gsljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('asl').addEventListener('click',()=>{
+
+    const note=document.getElementById('asljs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('cs').addEventListener('click',()=>{
+
+    const note=document.getElementById('csjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('ds').addEventListener('click',()=>{
+
+    const note=document.getElementById('dsjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('fs').addEventListener('click',()=>{
+
+    const note=document.getElementById('fsjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('gs').addEventListener('click',()=>{
+
+    const note=document.getElementById('gsjs')
+    stopSong(note);
+    note.play()
+})
+document.getElementById('as').addEventListener('click',()=>{
+
+    const note=document.getElementById('asjs')
+    stopSong(note);
+    note.play()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
